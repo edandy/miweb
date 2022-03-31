6e2548a3af9de2caf7e37e407a637cf2223a5e51
Dandy <edandy.perez@gmail.com>
2022-03-30 22:20:19 -0500

    eliminar 7

--- middleware/addTrailingSlash.js
+++ middleware/addTrailingSlash.js
@@ -1,7 +1,7 @@
 // middleware/addTrailingSlash.js
 export default function({ route, redirect }) {
-    if (route.path.slice(-1) !== '/') {
-      route.path += '/'
-      return redirect(route) // ルートオブジェクトを渡すことでパラメータやハッシュを残せる
+    if (route.path.slice(-1) === '/') {
+        route.path.slice(-1)
+        return redirect(route) // ルートオブジェクトを渡すことでパラメータやハッシュを残せる
     }
-  }
+}

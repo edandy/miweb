18fb4def127f367f9ffd94eb58eadde9c5b05a1f
Dandy <edandy.perez@gmail.com>
2023-08-30 01:00:19 -0500

    Cambiar de api

---
+++ pages/tv.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>
+            <iframe src="https://bludot.skin/dtvpl2.html?id=1251" frameborder="0" width="500" height="400"></iframe>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PageTv'
+    }
+</script>
--- store/contact.js
+++ store/contact.js
@@ -1,6 +1,6 @@
 const api = process.env.NUXT_ENV_STRAPI_ENDPOINT
 const initialState = {}
-  
+
 export const state = () => ({ ...initialState })
 
 export const getters = {
@@ -9,9 +9,9 @@ export const getters = {
 export const actions = {
   async sendForm(_, params) {
     try {
-      const res = await this.$axios.post(api + '/api/contacts', params)
+      const res = await this.$axios.post(api + '/contacts/store', params)
       if(res.status === 200) return Promise.resolve(res);
-    } catch (err) {          
+    } catch (err) {
       return Promise.reject(err);
     }
   }
